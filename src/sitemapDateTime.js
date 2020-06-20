2dd357f2d4b65b7bc87826dfb0182d5b6b1afbdc
Nuno Freitas <nunofreitas@gmail.com>
2020-06-20 14:48:02 +0100

    fix(lastmod): bail on empty date

--- src/sitemapDateTime.js
+++ src/sitemapDateTime.js
@@ -1,6 +1,10 @@
 "use strict";
 
 module.exports = (date) => {
+  if (!date) {
+    return;
+  }
+
   const dateObject = typeof date === "string" ? new Date(date) : date;
 
   return dateObject.toISOString();
