88b914cd8d702271681956e52ebeb2bdfbdce0ee
Robin Abrahamsson <robin@abrahamsson.tech>
2021-02-08 22:55:52 +0100

    feat(pagination): added pagination urls to sitemap

--- src/sitemapItems.js
+++ src/sitemapItems.js
@@ -3,7 +3,28 @@
 const sitemapItem = require("./sitemapItem");
 const sitemapProperty = require("./sitemapProperty");
 
+function isPagination(item) {
+  return (
+    item !== undefined &&
+    item.data !== undefined &&
+    item.data.pagination !== undefined &&
+    item.data.pagination.pages !== undefined
+  );
+}
+
+function getPaginationUrls(item) {
+  if (isPagination(item)) {
+    return item.data.pagination.pages.map((page) => ({
+      ...item,
+      url: page.url,
+    }));
+  }
+
+  return [item];
+}
+
 module.exports = (items, options) =>
   items
     .filter((item) => !sitemapProperty(item, "ignore") && item.url)
+    .flatMap(getPaginationUrls)
     .map((item) => sitemapItem(item, options));
