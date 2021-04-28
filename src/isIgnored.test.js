c6e41f8a9df22899fa62dbe6dcaf14a9e2752d13
Nuno Freitas <nunofreitas@gmail.com>
2021-04-28 16:10:00 +0100

    test: add isIgnored tests

---
+++ src/isIgnored.test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const test = require("ava");
+
+const isIgnored = require("./isIgnored");
+
+test("ignores items", (t) => {
+  t.true(isIgnored({ data: { sitemap: { ignore: true } } }));
+});
+
+test("ignores items without URL", (t) => {
+  t.true(isIgnored({}));
+});
+
+test("ignores empty items", (t) => {
+  t.true(isIgnored());
+});
