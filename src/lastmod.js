63f1e83b3020bc65787e453c7f68440ff38258ee
Nuno Freitas <nunofreitas@gmail.com>
2021-04-28 16:57:58 +0100

    fix(lastmod): undefined options

--- src/lastmod.js
+++ src/lastmod.js
@@ -3,10 +3,15 @@
 const sitemapDateTime = require("./sitemapDateTime");
 const sitemapProperty = require("./sitemapProperty");
 
-module.exports = (item, { lastModifiedProperty }) =>
-  sitemapProperty(item, "lastmod") ||
-  (lastModifiedProperty &&
-    item.data &&
-    item.data[lastModifiedProperty] &&
-    sitemapDateTime(item.data[lastModifiedProperty])) ||
-  sitemapDateTime(item.date);
+module.exports = (item, options) => {
+  const lastModifiedProperty = options && options.lastModifiedProperty;
+
+  return (
+    sitemapProperty(item, "lastmod") ||
+    (lastModifiedProperty &&
+      item.data &&
+      item.data[lastModifiedProperty] &&
+      sitemapDateTime(item.data[lastModifiedProperty])) ||
+    sitemapDateTime(item.date)
+  );
+};
